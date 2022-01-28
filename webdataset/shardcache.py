9c3c3b5b2bdccd27169e1b2adcae26140c39660f
Abhinav Venigalla <abhi@mosaicml.com>
2022-01-27 20:04:39 -0800

    patch shardcache

--- webdataset/shardcache.py
+++ webdataset/shardcache.py
@@ -75,8 +75,8 @@ class CacheStream(io.RawIOBase):
         """
         data = self.stream.read(n)
         self.cache.write(data)
-        if data is None or len(data) < n:
-            self.close(complete=True)
+        #if data is None or len(data) < n:
+        #    self.close(complete=True)
         self.last = ("read", n, len(data) if data is not None else None)
         return data
 
@@ -87,8 +87,8 @@ class CacheStream(io.RawIOBase):
         """
         n = self.stream.readinto(b)
         self.cache.write(b[:n])
-        if n == 0:
-            self.close(complete=True)
+        #if n == 0:
+        #    self.close(complete=True)
         self.last = ("readinto", n)
         return n
 
@@ -120,6 +120,7 @@ def cache_shards(urls, cache_dir="./data", cache_size=1e15, cache_name=guess_sha
         if not os.path.exists(cache_path):
             _cache = CacheStream(cache_path, stream, verbose=verbose)
             yield dict(url=url, stream=_cache)
+            _cache.close(complete=True)
         else:
             if verbose:
                 print("[opening cached", cache_path, "]", file=sys.stderr, flush=True)
